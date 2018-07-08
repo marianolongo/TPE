fe3e6db4d659801b690f07dda57a97e1ccaf4b09
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2018-07-08 07:32:45 -0300

    2.5

--- src/fileManager.c
+++ src/fileManager.c
@@ -11,18 +11,21 @@ provinceList readFile(char *fileName) {
 
     char line[100];
     char supLine[100];
+    while(fgets(supLine, 100, superficies)){
+        for (int i = 0; i <= 1; ++i) {
+            char *aux = strdup(supLine);
+            currentLineSup[i] = getField(aux, i + 1);
+        }
+        provinceStructPointer province = newProvince(currentLineSup[0]);
+        province->surface = atoi(currentLineSup[1]);
+        insertProvince(proList,province);
+    }
     while (fgets(line, 100, censo)) {
-        fgets(supLine, 100, superficies);
         for (int i = 0; i <= 3; ++i) {
             char *tmp = strdup(line);
             currentLineCenso[i] = getField(tmp, i + 1);
         }
-        for (int j = 0; j <= 1; ++j) {
-            char *aux = strdup(supLine);
-            currentLineSup[j] = getField(aux, j + 1);
-        }
         listElementProvince province = newProvince(currentLineCenso[3]);
-        if (provinceBelongs(proList, province) == 1) { //Si la tiene
             province = searchProvince(proList, province);
             listElementDepartment department = newDepartment(currentLineCenso[2]);
             if (departmentBelongs(province->departments, department) == 1) { //Si la tiene
@@ -40,29 +43,10 @@ provinceList readFile(char *fileName) {
                     increaseProvinceHomesAmount(province);
                 }
             }
-        } else {
-            insertProvince(proList, province);
-            listElementDepartment department = newDepartment(currentLineCenso[2]);
-            if (departmentBelongs(province->departments, department) == 1) { //Si la tiene
-                department = searchDepartment(province->departments, department);
-                increaseDepartmentPopulation(department, atoi(currentLineCenso[0]));
-                increaseProvincePopulation(province);
-                if(addHome(department,atoi(currentLineCenso[1])) == 1){ //Si agrego
-                    increaseProvinceHomesAmount(province);
-                }
-            } else {
-                insertDepartment(province->departments, department);
-                increaseDepartmentPopulation(department, atoi(currentLineCenso[0]));
-                increaseProvincePopulation(province);
-                if(addHome(department,atoi(currentLineCenso[1])) == 1){ //Si agrego
-                    increaseProvinceHomesAmount(province);
-                }
-            }
 //            printf("a");
-        }
     }
-    printf("a");
-
+//    printf("a");
+//
     for (int j = 0; j < listProvinceSize(proList); ++j) {
         provinceStructPointer province = getProvince(proList,j);
         printf("%i ", j);
